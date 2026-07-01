5988ad93a32cd62f61fa327e97d77fe59266976d
AmadorJuan <juanpablo243gg@gmail.com>
2026-07-01 17:49:01 -0300

    Modifique footer agregando mui

--- pv_tp_integrador_grupo12/src/components/layout/Footer.jsx
+++ pv_tp_integrador_grupo12/src/components/layout/Footer.jsx
@@ -1,21 +1,37 @@
-function Footer(){
-    return(
-        <footer>
-            <div>
-                <h3>Trabajo Integrador de Programación Visual</h3>
-                <div>
-                    <h3>Integrantes</h3>
-                    <ul>
-                        <li>Amador Juan Pablo - AmadorJuan</li>
-                        <li>Guanuco Marcos - MarcosGuanuco</li>
-                        <li>Torres Santiago - Santiago-Torres72</li>
-                    </ul>
-                </div>
-            </div>
-            <div>
-                <p>&copy; 2026 - Proyecto ProgramacionVisual</p>
-            </div>
-        </footer>
-    )
+import { Box, Typography, Stack } from "@mui/material";
+
+export default function Footer() {
+  return (
+    <Box
+      component="footer"
+      sx={{
+        py: 1,
+        background: "linear-gradient(to right, #141E30, #2c4766)",
+        color: "white",
+        textAlign: "center",
+      }}
+    >
+      <Stack spacing={2} alignItems="center">
+
+        <Typography variant="h6" fontWeight="bold">
+          Trabajo Integrador de Programación Visual
+        </Typography>
+
+        <Box>
+          <Typography variant="subtitle1" fontWeight="bold">
+            Integrantes
+          </Typography>
+
+          <Typography>Amador Juan Pablo - AmadorJuan</Typography>
+          <Typography>Guanuco Marcos - MarcosGuanuco</Typography>
+          <Typography>Torres Santiago - Santiago-Torres72</Typography>
+        </Box>
+
+        <Typography variant="body2" sx={{ opacity: 0.7 }}>
+          © 2026 - Proyecto Programación Visual
+        </Typography>
+
+      </Stack>
+    </Box>
+  );
 }
-export default Footer;
